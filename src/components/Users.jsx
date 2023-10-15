578b47feb33010a5cad2068f64e25c7b3c7fd48f
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-15 17:43:52 +0600

    delete operation done

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers)
     const handleDelete = _id=> {
         console.log("delete", _id);
+
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method:"DELETE"
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.deletedCount > 0){
+                Swal.fire({
+                    title: 'Good job!',
+                    text: 'Delete successfully.',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                  })
+                  const remaining = users.filter(user => user._id !== _id);
+                  setUsers(remaining);
+            }
+        })
     }
     return (
         <div>
